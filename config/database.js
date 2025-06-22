0f3bdd0ab4983eba43c9a37e07fe1e0ebff05aca
josaPlus <josafat.saldana@hotmial.comm>
2025-06-22 01:17:28 -0600

    conexion a base de datos

--- config/database.js
+++ config/database.js
@@ -1,11 +1,21 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
+console.log('DB Config:', {
+  host: process.env.HOST,
+  port: process.env.PORT,
+  user: process.env.DB_USER,
+  password: process.env.PASSWORD ? '***' : 'missing',
+  database: process.env.DATABASE
+});
+
+
 const connection = mysql.createConnection({
-    host: process.env.host || 'localhost',
-    user: process.env.username || 'root',
-    password: process.env.password || '',
-    database: process.env.database || 'test',
+    host: process.env.HOST,
+    port: process.env.PORT,
+    user: process.env.DB_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
 });
 
 module.exports = connection;
